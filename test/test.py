071593a35a380d71f847e6c137b861afd0356816
thanusit <72438474+thanusit@users.noreply.github.com>
2026-06-14 14:55:46 +0700

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,34 +1,11 @@
 # SPDX-FileCopyrightText: © 2024 Tiny Tapeout
 # SPDX-License-Identifier: Apache-2.0
 
-# import cocotb
-# from cocotb.clock import Clock
-# from cocotb.triggers import ClockCycles
-
-
-# @cocotb.test()
-# async def test_project(dut):
-#    dut._log.info("Start")
-
-    # Set the clock period to 10 us (100 KHz)
-#    clock = Clock(dut.clk, 10, unit="us")
-#    cocotb.start_soon(clock.start())
-
-    # Reset
-#    dut._log.info("Reset")
-#    dut.ena.value = 1
-#    dut.ui_in.value = 0
-#    dut.uio_in.value = 0
-#    dut.rst_n.value = 0
-#    await ClockCycles(dut.clk, 10)
-#   dut.rst_n.value = 1
-
-#    dut._log.info("Test project behavior")
-
 import cocotb
 from cocotb.triggers import Timer
 
 @cocotb.test()
+# To have the test entirely performed in pure verilog, configure cocotb to start and pause until tb.v is completed.
 async def run_verilog_for_fixed_duration(dut):
     """
     Waits for a predefined simulation timeframe to give 
@@ -38,17 +15,7 @@ async def run_verilog_for_fixed_duration(dut):
     dut.ena.value = 1
     # Adjust 10000 to match your testbench duration requirements
     await Timer(18990, unit="ns") 
-    #pass
-# Set the input values you want to test
-    #dut.ui_in.value = 20
-    #dut.uio_in.value = 30
 
-    # Wait for one clock cycle to see the output values
-    #await ClockCycles(dut.clk, 1)
-
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    #assert dut.uo_out.value == 50
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.

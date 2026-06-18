624e904491af86887bca792c1a3cc40dc7be74cf
thanusit <72438474+thanusit@users.noreply.github.com>
2026-06-18 10:46:27 +0700

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -107,7 +107,7 @@ module tb ();
 
         #200;
         $display("[TB] Simulation completed successfully.");
-        $finish;
+    //    $finish;
     end
   
    // Dump the signals to a FST file. You can view it with gtkwave or surfer.
